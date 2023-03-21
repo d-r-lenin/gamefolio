ef863d1d0ee2472feff9999b6187327b79569d63
d-r-lenin <drlenin2002@gmail.com>
2023-03-21 19:58:17 +0530

    init setup

---
+++ src/assets/SERENE_VILLAGE_REVAMPED/tilesets/village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="village" tilewidth="32" tileheight="32" tilecount="855" columns="19">
+ <image source="../Serene_Village_32x32.png" trans="aaaa7f" width="608" height="1440"/>
+</tileset>
